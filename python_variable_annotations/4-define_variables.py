a20f0bd499e18ea89eb6336f1376cfc4ca4a60fd
z502wa <104654178+z502wa@users.noreply.github.com>
2025-07-28 22:32:39 +0300

    Create 4-define_variables.py

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module: 4-define_variables
+Defines and annotates variables: a, pi, i_understand_annotations, and school.
+"""
+
+# Variable a is an integer with value of 1
+a: int = 1
+
+# Variable pi is a float with value of 3.14
+pi: float = 3.14
+
+# Variable i_understand_annotations is a boolean with value True
+i_understand_annotations: bool = True
+
+# Variable school is a string with value "Holberton"
+school: str = "Holberton"
