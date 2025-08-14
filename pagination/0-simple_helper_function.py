5573805b1b8176c2889ae5697b98c1d4e170ae22
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-14 17:08:19 +0300

    Update 0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 """
 suhail alaboud
 """
-def index_range(page: int, page_size: int) -> tuble:
-  start_index = page - 1) * page_size
-  end_index = start_index + page_size
-  return (start_index, end_index)
+def index_range(page: int, page_size: int) -> tuple:
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
   
