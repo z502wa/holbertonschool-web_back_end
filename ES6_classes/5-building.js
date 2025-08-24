ad3f9edd35668598f3b21c342e5cfa92078ce5f3
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-24 19:06:32 +0300

    Create 5-building.js

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+
+    if (this.constructor !== Building
+        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
