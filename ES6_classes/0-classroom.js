6e69f3da3f70293fca5a1e8b9c98a4954bb50575
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-24 16:10:35 +0300

    Update 0-classroom.js

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,9 +1,10 @@
+// Suhail Al-aboud - 10675@holbertonstudents.com
+
+// Define a Class named ClassRoom
 export default class ClassRoom {
+  // Constructor that accepts one argument: maxStudentsSize
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
-}export default class ClassRoom {
-  constructor(maxStudentsSize) {
+    // Assign the value to the private property _maxStudentsSize
     this._maxStudentsSize = maxStudentsSize;
   }
 }
