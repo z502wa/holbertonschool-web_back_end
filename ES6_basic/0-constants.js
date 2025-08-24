b7062cbbd95564faf6982f4c64667f335bb0badf
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-24 15:48:22 +0300

    Update 0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,20 +1,14 @@
-// Suhail Al-aboud - 10675@holbertonstudents.com
-
-// Function that uses const to declare a variable (cannot be reassigned)
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Function that returns a static string
 export function getLast() {
   return ' is okay';
 }
 
-// Function that uses let to declare a variable (can be reassigned or modified)
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += getLast(); // Append the result of getLast()
-
+  combination += getLast();
   return combination;
 }
