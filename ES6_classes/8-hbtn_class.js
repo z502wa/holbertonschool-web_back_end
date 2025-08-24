9945f63df39d0d5079f87c91616bf090637ad0a6
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-24 19:08:38 +0300

    Create 8-hbtn_class.js

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,23 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this._location;
+  }
+}
