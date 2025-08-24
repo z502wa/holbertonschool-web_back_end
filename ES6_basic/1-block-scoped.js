16e69d792ce3368049ba87c5d8b60e72beaf5c2d
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-24 15:52:15 +0300

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,16 +1,13 @@
-// Suhail Al-aboud - 10675@holbertonstudents.com
-
-// Function demonstrating block scope using let and const
 export default function taskBlock(trueOrFalse) {
-  const task = false;   // Using const since this value should not change
-  const task2 = true;   // Same here, remains constant
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    // Variables inside this block will not overwrite the outer variables
-    const task = true;   // Scoped only inside the if block
-    const task2 = false; // Scoped only inside the if block
+    (function() {
+      var task = true;
+      var task2 = false;
+    })();
   }
 
-  // Returns the values defined in the outer scope (not affected by block variables)
   return [task, task2];
 }
