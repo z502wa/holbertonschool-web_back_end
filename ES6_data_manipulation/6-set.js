f0d0b4cb2146ea363bc02c329d5d36224553aab9
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-31 19:54:18 +0300

    Create 6-set.js

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Suhail Al-aboud
+ * Email: 10675@holbertonstudents.com
+ */
+
+ // Function that creates a Set from an array
+export default function setFromArray(array) {
+  // Create a new Set using the array argument
+  return new Set(array);
+}
