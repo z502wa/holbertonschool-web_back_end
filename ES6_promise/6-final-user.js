c76fff32092b2e05c3d0bb8fed633c112e9476e8
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-31 20:08:55 +0300

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,10 +6,17 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-// Function that handles multiple promises and returns their settled results
+// Function that handles multiple promises and normalizes results to { status, value }
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) =>
+    results.map((res) => (
+      res.status === 'fulfilled'
+        ? { status: res.status, value: res.value }
+        // Convert Error to the expected string "Error: <message>"
+        : { status: res.status, value: String(res.reason) }
+    )),
+  );
 }
