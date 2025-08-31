7f884d0388ed269161309660471ac320104f7979
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-31 20:01:08 +0300

    Create 0-promise.js

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+/**
+ * Author: Suhail Al-aboud
+ * Email: 10675@holbertonstudents.com
+ */
+
+ // Function that returns a Promise
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Here you can resolve immediately or handle async logic
+    resolve();
+  });
+}
