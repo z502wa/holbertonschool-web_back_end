22ecc98e4dbd52347e37aa96d9e2e0e63c6cc58e
z502wa <104654178+z502wa@users.noreply.github.com>
2025-08-31 19:49:15 +0300

    Create 0-get_list_students.js

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,14 @@
+/**
+ * Author: Suhail Al-aboud
+ * Email: 10675@holbertonstudents.com
+ */
+
+ // Function that returns a list of student objects
+export default function getListStudents() {
+  // Create an array of students with id, firstName, and location
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
