76b4b904ac02a90528d025201b721930019ecd03
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 23:18:47 +0300

    Update AppController.js

--- Node_JS_basic/full_server/controllers /AppController.js
+++ Node_JS_basic/full_server/controllers /AppController.js
@@ -1,5 +1,19 @@
+/*
+ * Author: Suhail Al-aboud
+ * Email: 10675@holbertonstudents.com
+ * File: full_server/controllers/AppController.js
+ * Description: Basic controller for the home route.
+ */
+
+'use strict';
+
 class AppController {
-  static getHomepage(req, res) {
+  /**
+   * GET /
+   * @param {import('express').Request} _req
+   * @param {import('express').Response} res
+   */
+  static getHomepage(_req, res) {
     res.status(200).send('Hello Holberton School!');
   }
 }
