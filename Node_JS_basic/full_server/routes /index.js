30f51ac5fa9722df08354700c72878e8e7a44379
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 23:19:31 +0300

    Update index.js

--- Node_JS_basic/full_server/routes /index.js
+++ Node_JS_basic/full_server/routes /index.js
@@ -1,11 +1,23 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
-import StudentsController from '../controllers/StudentsController';
+/*
+ * Author: Suhail Al-aboud
+ * Email: 10675@holbertonstudents.com
+ * File: full_server/routes/index.js
+ * Description: Express router wiring App and Students controllers.
+ */
 
-const router = express.Router();
+'use strict';
 
-router.get('/', (req, res) => AppController.getHomepage(req, res));
-router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
-router.get('/students/:major', (req, res) => StudentsController.getAllStudentsByMajor(req, res));
+import { Router } from 'express';
+import AppController from '../controllers/AppController.js';
+import StudentsController from '../controllers/StudentsController.js';
+
+const router = Router();
+
+// Home
+router.get('/', AppController.getHomepage);
+
+// Students
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 export default router;
