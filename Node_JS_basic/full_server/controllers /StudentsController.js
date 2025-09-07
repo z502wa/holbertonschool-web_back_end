fe1c2f906a1154c9553d541791f6d5f91aed6eb0
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 23:44:41 +0300

    Update StudentsController.js

--- Node_JS_basic/full_server/controllers /StudentsController.js
+++ Node_JS_basic/full_server/controllers /StudentsController.js
@@ -1,62 +1,47 @@
-/*
- * Author: Suhail Al-aboud
- * Email: 10675@holbertonstudents.com
- * File: full_server/controllers/StudentsController.js
- * Description: Controller for students listing endpoints.
- */
-
-'use strict';
-
-import readDatabase from '../utils.js';
+import readDatabase from '../utils';
 
 class StudentsController {
-  /**
-   * GET /students
-   * - First line: "This is the list of our students"
-   * - For each field (alphabetically, case-insensitive):
-   *   "Number of students in FIELD: N. List: name1, name2, ..."
-   */
-  static async getAllStudents(_req, res) {
-    try {
-      // Retrieve DB path at execution time (not at require-time), as required by the checker
-      const dbPath = process.argv[2];
-      const map = await readDatabase(dbPath);
+  static getAllStudents(request, response, DATABASE) {
+    readDatabase(DATABASE)
+      .then((fields) => {
+        const students = [];
+        // let count = 0;
+        let msg;
 
-      const header = 'This is the list of our students';
-      const fields = Object.keys(map).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        // for (const key of Object.keys(fields)) {
+        //   count += fields[key].length;
+        // }
 
-      const lines = [header];
-      for (const field of fields) {
-        const list = map[field] || [];
-        lines.push(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-      }
+        // students.push(`Number of students: ${count}`);
+        students.push('This is the list of our students');
 
-      res.status(200).send(lines.join('\n'));
-    } catch (_err) {
-      res.status(500).send('Cannot load the database');
-    }
+        for (const key of Object.keys(fields)) {
+          msg = `Number of students in ${key}: ${
+            fields[key].length
+          }. List: ${fields[key].join(', ')}`;
+
+          students.push(msg);
+        }
+        response.send(200, `${students.join('\n')}`);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
+      });
   }
 
-  /**
-   * GET /students/:major
-   * - major must be "CS" or "SWE"
-   * - Response: "List: name1, name2, ..."
-   */
-  static async getAllStudentsByMajor(req, res) {
-    try {
-      const { major } = req.params;
-      if (major !== 'CS' && major !== 'SWE') {
-        res.status(500).send('Major parameter must be CS or SWE');
-        return;
-      }
+  static getAllStudentsByMajor(request, response, DATABASE) {
+    const { major } = request.params;
 
-      const dbPath = process.argv[2];
-      const map = await readDatabase(dbPath);
-      const list = map[major] || [];
+    if (major !== 'CS' && major !== 'SWE') {
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      readDatabase(DATABASE)
+        .then((fields) => {
+          const students = fields[major];
 
-      res.status(200).send(`List: ${list.join(', ')}`);
-    } catch (_err) {
-      res.status(500).send('Cannot load the database');
+          response.send(200, `List: ${students.join(', ')}`);
+        })
+        .catch(() => response.send(500, 'Cannot load the database'));
     }
   }
 }
