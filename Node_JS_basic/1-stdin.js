4d743f059e6593d810978c09b35009ef67478da4
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 18:19:37 +0300

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,10 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (input) => {
-  const name = input.trim(); // إزالة الفراغات مثل \n
+  const name = input.trim();
   console.log(`Your name is: ${name}`);
 });
 
-process.on('exit', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
