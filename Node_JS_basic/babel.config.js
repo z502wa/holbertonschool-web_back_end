c010562d5ee3eb0d67dc1c40cab106d4af63bb0d
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 23:21:27 +0300

    Update babel.config.js

--- Node_JS_basic/babel.config.js
+++ Node_JS_basic/babel.config.js
@@ -1,11 +1,18 @@
+/*
+ * Author: Suhail Al-aboud
+ * Email: 10675@holbertonstudents.com
+ * File: babel.config.js
+ * Description: Babel config enabling ES6 on current Node.
+ */
+
+'use strict';
+
 module.exports = {
   presets: [
     [
       '@babel/preset-env',
       {
-        targets: {
-          node: 'current',
-        },
+        targets: { node: 'current' },
       },
     ],
   ],
