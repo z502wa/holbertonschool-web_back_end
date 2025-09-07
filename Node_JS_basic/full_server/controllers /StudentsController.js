d27afe89e5a6c6b18e9babfeabb886f3435bee2d
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 23:45:47 +0300

    Update StudentsController.js

--- Node_JS_basic/full_server/controllers /StudentsController.js
+++ Node_JS_basic/full_server/controllers /StudentsController.js
@@ -1,22 +1,49 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
-import StudentsController from '../controllers/StudentsController';
+import readDatabase from '../utils';
 
-function controllerRouting(app) {
-  const router = express.Router();
-  app.use('/', router);
+class StudentsController {
+  static getAllStudents(request, response, DATABASE) {
+    readDatabase(DATABASE)
+      .then((fields) => {
+        const students = [];
+        // let count = 0;
+        let msg;
 
-  router.get('/', (req, res) => {
-    AppController.getHomepage(req, res);
-  });
+        // for (const key of Object.keys(fields)) {
+        //   count += fields[key].length;
+        // }
 
-  router.get('/students', (req, res) => {
-    StudentsController.getAllStudents(req, res, process.argv[2]);
-  });
+        // students.push(`Number of students: ${count}`);
+        students.push('This is the list of our students');
 
-  router.get('/students/:major', (req, res) => {
-    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
-  });
+        for (const key of Object.keys(fields)) {
+          msg = `Number of students in ${key}: ${
+            fields[key].length
+          }. List: ${fields[key].join(', ')}`;
+
+          students.push(msg);
+        }
+        response.send(200, `${students.join('\n')}`);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response, DATABASE) {
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      readDatabase(DATABASE)
+        .then((fields) => {
+          const students = fields[major];
+
+          response.send(200, `List: ${students.join(', ')}`);
+        })
+        .catch(() => response.send(500, 'Cannot load the database'));
+    }
+  }
 }
 
-export default controllerRouting;
+export default StudentsController;
