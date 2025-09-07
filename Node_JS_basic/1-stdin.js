57f4be3af4ad3220f9c552e49fab6412013d0d02
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 20:04:36 +0300

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,8 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (userInput) => {
-  process.stdout.write(`Your name is: ${userInput}`);
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+});
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
