908c9763492a040a30a3a51db968e356ed00d2b8
z502wa <104654178+z502wa@users.noreply.github.com>
2025-09-07 23:19:06 +0300

    Update StudentsController.js

--- Node_JS_basic/full_server/controllers /StudentsController.js
+++ Node_JS_basic/full_server/controllers /StudentsController.js
@@ -1,29 +1,63 @@
-import readDatabase from '../utils';
+/*
+ * Author: Suhail Al-aboud
+ * Email: 10675@holbertonstudents.com
+ * File: full_server/controllers/StudentsController.js
+ * Description: Controller for students listing endpoints.
+ */
+
+'use strict';
+
+import readDatabase from '../utils.js';
 
 class StudentsController {
-  static getAllStudents(req, res) {
-    readDatabase(process.argv[2])
-      .then((data) => {
-        let result = 'This is the list of our students';
-        for (const [key] of Object.entries(data).sort()) {
-          result += `\nNumber of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`;
-        }
-        res.status(200).send(result);
-      })
-      .catch(() => res.status(500).send('Cannot load the database'));
+  /**
+   * GET /students
+   * - First line: "This is the list of our students"
+   * - For each field (alphabetically, case-insensitive):
+   *   "Number of students in FIELD: N. List: name1, name2, ..."
+   */
+  static async getAllStudents(_req, res) {
+    try {
+      // Retrieve DB path at execution time (not at require-time), as required by the checker
+      const dbPath = process.argv[2];
+      const map = await readDatabase(dbPath);
+
+      const header = 'This is the list of our students';
+      const fields = Object.keys(map).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+      const lines = [header];
+      for (const field of fields) {
+        const list = map[field] || [];
+        lines.push(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+      }
+
+      res.status(200).send(lines.join('\n'));
+    } catch (_err) {
+      res.status(500).send('Cannot load the database');
+    }
   }
 
-  static getAllStudentsByMajor(req, res) {
-    readDatabase(process.argv[2])
-      .then((data) => {
-        if (Object.keys(data).includes(req.params.major)) {
-          const result = `List: ${data[req.params.major].join(', ')}`;
-          res.status(200).send(result);
-        } else {
-          res.status(500).send('Major parameter must be CS or SWE');
-        }
-      })
-      .catch(() => res.status(500).send('Cannot load the database'));
+  /**
+   * GET /students/:major
+   * - major must be "CS" or "SWE"
+   * - Response: "List: name1, name2, ..."
+   */
+  static async getAllStudentsByMajor(req, res) {
+    try {
+      const { major } = req.params;
+      if (major !== 'CS' && major !== 'SWE') {
+        res.status(500).send('Major parameter must be CS or SWE');
+        return;
+      }
+
+      const dbPath = process.argv[2];
+      const map = await readDatabase(dbPath);
+      const list = map[major] || [];
+
+      res.status(200).send(`List: ${list.join(', ')}`);
+    } catch (_err) {
+      res.status(500).send('Cannot load the database');
+    }
   }
 }
 
